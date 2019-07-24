0400dcc48973a38d8ced6b743ecc8716e8ac2e64
fc <15225488241@163.com>
2019-07-24 19:37:25 +0800

    微信公众号框架

---
+++ src/main/java/com/fc/constans/MenuConstans.java
@@ -0,0 +1,85 @@
+package com.fc.constans;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MenuConstans {
+    enum MenuKey{
+        ONE("00001","music"),
+        TWO("00002","video"),
+        THREE("00003","photo"),;
+        MenuKey(String key,String value){
+            this.key=key;
+            this.value=value;
+        }
+        private String key;
+        private String value;
+
+        public String getKey() {
+            return key;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+    public static final String WX_MENU;
+            static {
+                Map<String,List> menu=new HashMap();
+                List<Map<String,Object>> menuList=new ArrayList<>();
+                //左侧菜单设置
+                menuList.add(new HashMap<String,Object>(){{
+                    this.put("name","左侧菜单");
+                    this.put("type","click");
+                    this.put("key","fc001");
+//                    List<Map<String,Object>> firstList=new ArrayList<>();
+//                    firstList.add(new HashMap<String,Object>(){{
+//                        this.put("type","click");
+//                        this.put("name","music");
+//                        this.put("key","fc001");
+//                    }});
+//                    firstList.add(new HashMap<String,Object>(){{
+//                        this.put("type","click");
+//                        this.put("name",MenuKey.TWO.getValue());
+//                        this.put("key",MenuKey.TWO.getKey());
+//                    }});
+//                    firstList.add(new HashMap<String,Object>(){{
+//                        this.put("type","click");
+//                        this.put("name",MenuKey.THREE.getValue());
+//                        this.put("key",MenuKey.THREE.getKey());
+//                    }});
+//                    this.put("sub_button",firstList);
+                }});
+//                //中间菜单设置
+//                menuList.add(new HashMap<String,Object>(){{
+//                    this.put("name","中间菜单");
+//                    List<Map<String,Object>> secondList=new ArrayList<>();
+//                    secondList.add(new HashMap<String,Object>(){{
+//                        this.put("type","view");
+//                        this.put("name","小说推荐");
+//                        this.put("url","https://www.baidu.com");
+//                    }});
+//                    this.put("sub_button",secondList);
+//                }});
+//                //右侧菜单设置
+//                menuList.add(new HashMap<String,Object>(){{
+//                    this.put("name","右边菜单");
+//                    List<Map<String,Object>> thirdList=new ArrayList<>();
+//                    thirdList.add(new HashMap<String,Object>(){{
+//                        this.put("type","view");
+//                        this.put("name","关于fc");
+//                        this.put("url","https://www.baidu.com");
+//                    }});
+//                    this.put("sub_button",thirdList);
+//                }});
+                menu.put("button",menuList);
+                WX_MENU= JSONObject.toJSONString(menu);
+            }
+    public static void main(String[] args) {
+        System.out.println(WX_MENU);
+    }
+}
--- src/main/java/com/fc/controller/Welcome.java
+++ src/main/java/com/fc/controller/Welcome.java
@@ -1,14 +1,73 @@
 package com.fc.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.fc.constans.MenuConstans;
+import com.fc.exception.AesException;
+import com.fc.utils.HttpUtils;
+import com.fc.utils.SHA1;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Map;
 
 @RequestMapping("welcome")
 @Controller
 public class Welcome {
+    /**微信token**/
+    private static final String WINXIN_TOKEN = "123456.";
+    /**appID**/
+    private static final String APP_ID = "wxdebe9ef3b8438a7e";
+    /**appsecret**/
+    private static final String APP_SECRET = "de910cf4eaa85af601197e6c33bc1a4f";
+    /**菜单请求地址**/
+    private static final String MENU_URL="https://api.weixin.qq.com/cgi-bin/menu/create?access_token=";
+    /*请求获取token地址*/
+    private static final String TOKEN_GET_RUL="https://api.weixin.qq.com/cgi-bin/token";
+
     @RequestMapping("welcome")
-    public ModelAndView welcome(){
+    public ModelAndView welcome() {
         return new ModelAndView("welcome");
+    }
+
+    @RequestMapping(value = "/wx/token",method = RequestMethod.GET)
+    @ResponseBody
+    public String verifyWXToken(HttpServletRequest request) throws AesException {
+        String msgSignature = request.getParameter("signature");
+        String msgTimestamp = request.getParameter("timestamp");
+        String msgNonce = request.getParameter("nonce");
+        String echostr = request.getParameter("echostr");
+        String signature = SHA1.getSHA1(WINXIN_TOKEN, msgTimestamp, msgNonce);
+        if (!signature.equals(msgSignature)) {
+            throw new AesException(AesException.ValidateSignatureError);
+        }
+        return echostr;
+
+    }
+    @RequestMapping(value = "/wx/token",method = RequestMethod.POST)
+    @ResponseBody
+    public void wxPost(HttpServletRequest request) throws Exception {
+        // 处理接收消息
+        ServletInputStream in = request.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String str=null;
+        StringBuffer sb=new StringBuffer();
+        while ((str=br.readLine())!=null){
+            sb.append(str);
+        }
+        System.out.println(sb.toString());
+    }
+    @RequestMapping("/wx/menu")
+    @ResponseBody
+    public String wxMenu(HttpServletRequest request) throws Exception {
+        String token = HttpUtils.sendGet(TOKEN_GET_RUL, "grant_type=client_credential&appid=" + APP_ID + "&secret=" + APP_SECRET);
+        String tk= JSONObject.parseObject(token, Map.class).get("access_token").toString();
+        return HttpUtils.sendPost( MENU_URL+tk,MenuConstans.WX_MENU);
     }
 }
